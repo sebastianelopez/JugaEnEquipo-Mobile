bcf3e48fdd6b42279f90cd5bf2dae1806fba8924
sebastianelopez <lopezsebastian.emanuel@gmail.com>
2024-04-13 16:28:07 -0300

    added main navigations routes

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -1,17 +1,12 @@
 import 'package:flutter/material.dart';
 
 import 'package:jugaenequipo/models/models.dart';
-import 'package:jugaenequipo/screens/screens.dart';
+import 'package:jugaenequipo/presentation/screens.dart';
 
 class AppRoutes {
   static const initialRoute = 'login';
 
   static final menuOptions = <MenuOption>[
-    MenuOption(
-        route: 'home',
-        name: 'Alert Screen',
-        screen: const HomeScreen(),
-        icon: Icons.list_alt_outlined),
     MenuOption(
         route: 'register',
         name: 'Register Screen',
@@ -21,7 +16,22 @@ class AppRoutes {
         route: 'login',
         name: 'Login Screen',
         screen: const LoginScreen(),
-        icon: Icons.list_alt_outlined)
+        icon: Icons.list_alt_outlined),
+  ];
+
+  static final mainNavigationOptions = <MainNavigationOption>[
+    MainNavigationOption(
+        route: 'home', screen: const HomeScreen(), icon: Icons.home),
+    MainNavigationOption(
+        route: 'teams',
+        screen: const TeamsScreen(),
+        icon: Icons.supervisor_account),
+    MainNavigationOption(
+        route: 'teams', screen: const TeamsScreen(), icon: Icons.add_circle),
+    MainNavigationOption(
+        route: 'tournaments', screen: const TournamentsScreen(), icon: Icons.emoji_events),
+    MainNavigationOption(
+        route: 'notifications', screen: const NotificationsScreen(), icon: Icons.notifications)
   ];
 
   static Map<String, Widget Function(BuildContext)> getAppRoutes() {
@@ -30,6 +40,10 @@ class AppRoutes {
     appRoutes.addAll({'login': (BuildContext context) => const LoginScreen()});
 
     for (final option in menuOptions) {
+      appRoutes.addAll({option.route: (BuildContext context) => option.screen});
+    }
+
+    for (final option in mainNavigationOptions) {
       appRoutes.addAll({option.route: (BuildContext context) => option.screen});
     }
 
