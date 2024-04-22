dc08773e5d3dd20ae109fc2f5ea979a3694319e4
sebastianelopez <lopezsebastian.emanuel@gmail.com>
2024-04-22 20:47:12 -0300

    make input responsive

--- lib/widgets/navbar/main_navbar.dart
+++ lib/widgets/navbar/main_navbar.dart
@@ -20,35 +20,39 @@ class MainNavbar extends StatelessWidget {
       ),
       shadowColor: Colors.black,
       actions: [
-        Container(
-          width: 273,
-          margin: const EdgeInsets.symmetric(vertical: 10.0),
-          decoration: const BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.all(
-              Radius.circular(10.0),
+        Row(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width * 0.7,
+              margin: const EdgeInsets.symmetric(vertical: 10.0),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10.0),
+                ),
+              ),
+              child: TextField(
+                decoration: const InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(vertical: 0.0),
+                  hintText: 'Search',
+                  border: InputBorder.none,
+                  enabledBorder: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                  prefixIcon: Icon(Icons.search),
+                ),
+                onChanged: (value) {
+                  // Handle the search input changes here.
+                },
+              ),
             ),
-          ),
-          child: TextField(
-            decoration: const InputDecoration(
-              contentPadding: EdgeInsets.symmetric(vertical: 0.0),
-              hintText: 'Search',
-              border: InputBorder.none,
-              enabledBorder: InputBorder.none,
-              focusedBorder: InputBorder.none,
-              prefixIcon: Icon(Icons.search),
+            IconButton(
+              color: Colors.white,
+              icon: const Icon(Icons.message),
+              onPressed: () {
+                // Handle the message icon press here.
+              },
             ),
-            onChanged: (value) {
-              // Handle the search input changes here.
-            },
-          ),
-        ),
-        IconButton(
-          color: Colors.white,
-          icon: const Icon(Icons.message),
-          onPressed: () {
-            // Handle the message icon press here.
-          },
+          ],
         ),
       ],
     );
