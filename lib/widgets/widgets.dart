36a69f5a0f85dbfd1ef8f26bbf70c5f9f63c699c
sebastianelopez <lopezsebastian.emanuel@gmail.com>
2024-05-09 23:52:01 -0300

    renamed folder to a more descriptive name

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,6 +1,8 @@
+export 'package:jugaenequipo/widgets/navigation/back_appbar.dart';
+export 'package:jugaenequipo/widgets/language_dropdown.dart';
 export 'package:jugaenequipo/widgets/drawer_nav.dart';
 export 'package:jugaenequipo/widgets/loading_icon.dart';
-export 'package:jugaenequipo/widgets/navbar/main_navbar.dart';
+export 'package:jugaenequipo/widgets/navigation/main_navbar.dart';
 export 'package:jugaenequipo/widgets/navigation/bottom_navigation_bar.dart';
 export 'package:jugaenequipo/widgets/auth_background.dart';
 export 'package:jugaenequipo/widgets/card_container.dart';
