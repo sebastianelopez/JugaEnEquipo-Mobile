313e4da18bc128537d3f4da1f522c49bc47a5fb9
sebastianelopez <lopezsebastian.emanuel@gmail.com>
2024-08-22 21:19:44 -0300

    add image detail screen

---
+++ lib/presentation/imageDetail/business_logic/image_detail_provider.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:jugaenequipo/models/models.dart';
+
+class ImageDetailProvider extends ChangeNotifier {
+  List<ChatUser> chatsMocks = [
+    ChatUser(
+        user: User(
+            id: "asdasd98",
+            firstName: "Lautaro",
+            lastName: "Rivadeneira",
+            userName: "LRGC",
+            email: "",
+            profileImage:
+                "https://scontent.cdninstagram.com/v/t51.2885-19/195780402_1055586888183094_99043525364029635_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent.cdninstagram.com&_nc_cat=110&_nc_ohc=XT8Rspy1K-EQ7kNvgGA1T9o&edm=APs17CUBAAAA&ccb=7-5&oh=00_AfD4gI3ZTZvVUa-z2ETq6fOTk6bqrFpoOa8G_hBi8xfz4A&oe=663C6879&_nc_sid=10d13b"),
+        messageText: "extraño tu pene",
+        isMessageRead: false,
+        time: "2024-05-04 15:00:04Z"),
+    ChatUser(
+        user: User(
+            id: "asda5sd498",
+            firstName: "Alejandro",
+            lastName: "Minetti",
+            userName: "Alem87",
+            email: "",
+            profileImage:
+                "https://scontent.cdninstagram.com/v/t51.2885-19/429985363_704173345123485_6621486499715753530_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent.cdninstagram.com&_nc_cat=109&_nc_ohc=K-zt1QFWQX8Q7kNvgGDhGBd&edm=APs17CUBAAAA&ccb=7-5&oh=00_AfCjp7AsUWD1zbcgb-tHrJmlv2wtfymnZ_yap1ebUhVnCQ&oe=663C6418&_nc_sid=10d13b"),
+        messageText: "me pasas tu skincare?",
+        isMessageRead: false,
+        time: "2024-04-21 19:18:04Z"),
+    ChatUser(
+        user: User(
+            id: "asda5sd128",
+            firstName: "Matias",
+            lastName: "Racing",
+            userName: "Matcing",
+            email: "",
+            profileImage:
+                "https://scontent.cdninstagram.com/v/t51.2885-19/57116481_405440056941131_5387659121300340736_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent.cdninstagram.com&_nc_cat=111&_nc_ohc=KYEc7Zv7PDsQ7kNvgGxupVR&edm=APs17CUBAAAA&ccb=7-5&oh=00_AfBmMXLt72pxPH3-8a-9qa97FE8T7bzQE7HWYXxNiA_1dA&oe=663C5A24&_nc_sid=10d13b"),
+        messageText: "soy bostero, no lo puedo ocultar mas",
+        isMessageRead: false,
+        time: "2024-03-21 19:18:04Z"),
+  ];
+}
---
+++ lib/presentation/imageDetail/screens/image_detail_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:jugaenequipo/widgets/widgets.dart';
+
+class ImageDetailScreen extends StatelessWidget {
+  final List<String> imageUrls;
+  final int currentIndex;
+
+  const ImageDetailScreen(
+      {Key? key, required this.imageUrls, required this.currentIndex})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: const PreferredSize(
+        preferredSize: Size(double.infinity, 40),
+        child: BackAppBar(
+          label: "",
+        ),
+      ),
+      body: CarouselSlider(
+        
+        items: imageUrls.map((imageUrl) {
+          return Builder(
+            builder: (context) => Hero(
+              tag: imageUrl,
+              child: Image.network(
+                imageUrl,
+                fit: BoxFit.contain,
+              ),
+            ),
+          );
+        }).toList(),
+        options: CarouselOptions(
+          height: MediaQuery.of(context).size.height, 
+          viewportFraction: 1.0, 
+          enableInfiniteScroll: false,
+          autoPlay: false, 
+          scrollDirection: Axis.horizontal,
+          initialPage: currentIndex
+        ),
+      ),
+    );
+  }
+}
