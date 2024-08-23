5a6639dbe2831b733f900da2dc2b1c0e24943b35
sebastianelopez <lopezsebastian.emanuel@gmail.com>
2024-08-22 21:22:07 -0300

    update comment model to match api response

--- lib/models/comment.dart
+++ lib/models/comment.dart
@@ -4,8 +4,16 @@ class Comment {
   final User user;
   final String? copy;
   final String? image;
-  final String commentDate;
+  final String createdAt;
+  final String updatedAt;
+  final String deletedAt;
 
-  Comment(
-      {required this.user, this.copy, this.image, required this.commentDate});
+  Comment({
+    required this.user,
+    this.copy,
+    this.image,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.deletedAt,
+  });
 }
