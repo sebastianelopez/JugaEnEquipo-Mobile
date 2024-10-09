2f0a6bc66e8047c11e59ce8720c215adca09ef87
sebastianelopez <lopezsebastian.emanuel@gmail.com>
2024-10-09 13:36:02 -0300

    add custom page view

---
+++ lib/global_widgets/custom_page_view.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:jugaenequipo/providers/providers.dart';
+import 'package:provider/provider.dart';
+
+class CustomPageView extends StatelessWidget {
+  final List<Widget> children;
+
+  const CustomPageView({
+    Key? key,
+    required this.children,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final navigation = Provider.of<NavigationProvider>(context);
+    return PageView(
+      controller: navigation.pageController,
+      physics: const NeverScrollableScrollPhysics(),
+      children: children,
+    );
+  }
+}
