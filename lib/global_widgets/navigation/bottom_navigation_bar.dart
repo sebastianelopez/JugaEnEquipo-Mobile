12c6b86d005e6c7703640024d5e8d9bf919f7399
sebastianelopez <lopezsebastian.emanuel@gmail.com>
2024-10-09 13:35:36 -0300

    add navigation provider

--- lib/global_widgets/navigation/bottom_navigation_bar.dart
+++ lib/global_widgets/navigation/bottom_navigation_bar.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:jugaenequipo/providers/providers.dart';
 import 'package:jugaenequipo/router/app_routes.dart';
 import 'package:jugaenequipo/global_widgets/create_post.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:jugaenequipo/theme/app_theme.dart';
+import 'package:provider/provider.dart';
 
 class BottomNavigationBarCustom extends StatefulWidget {
   const BottomNavigationBarCustom({Key? key}) : super(key: key);
@@ -11,58 +14,49 @@ class BottomNavigationBarCustom extends StatefulWidget {
 }
 
 class _BottomNavigationBarState extends State<BottomNavigationBarCustom> {
-  int _selectedIndex = 0;
-
   final mainNavigationOptions = AppRoutes.mainNavigationOptions;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-    if (mainNavigationOptions[index].route == "notifications") {
-      FocusScope.of(context).unfocus();
-      Navigator.pushNamed(context, mainNavigationOptions[index].route);
-    } else if (mainNavigationOptions[index].route == "createPost") {
-      showModalBottomSheet(
-        context: context,
-        constraints: BoxConstraints(
-          maxHeight: 600.h,
-          maxWidth: 1200,
-        ),
-        isScrollControlled: true,
-        useSafeArea: true,
-        builder: (BuildContext context) {
-          return const CreatePost();
-        },
-      );
-    } else {
-      Navigator.pushReplacement(
-        context,
-        PageRouteBuilder(
-          pageBuilder: (context, animation1, animation2) =>
-              mainNavigationOptions[index].screen!,
-          transitionDuration: Duration.zero,
-          reverseTransitionDuration: Duration.zero,
-        ),
-      );
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
+    final navigation = Provider.of<NavigationProvider>(context);
+
+    void onItemTapped(int index) {
+      if (mainNavigationOptions[index].route == "notifications") {
+        FocusScope.of(context).unfocus();
+        Navigator.pushNamed(context, mainNavigationOptions[index].route);
+      } else if (mainNavigationOptions[index].route == "createPost") {
+        showModalBottomSheet(
+          context: context,
+          constraints: BoxConstraints(
+            maxHeight: 600.h,
+            maxWidth: 1200,
+          ),
+          isScrollControlled: true,
+          useSafeArea: true,
+          builder: (BuildContext context) {
+            return const CreatePost();
+          },
+        );
+      } else {
+        navigation.setCurrentPage = index;
+      }
+    }
+
     return BottomNavigationBar(
       type: BottomNavigationBarType.fixed,
       showSelectedLabels: false,
       showUnselectedLabels: false,
-      currentIndex: _selectedIndex,
+      currentIndex: navigation.currentPage,
+      onTap: (i) => onItemTapped(i),
       selectedItemColor: Colors.red[900],
-      onTap: _onItemTapped,
       items: mainNavigationOptions.map((option) {
         return BottomNavigationBarItem(
           icon: Icon(
             size: 24.h,
             option.icon,
-            color: const Color.fromARGB(255, 1, 15, 36),
+            color: mainNavigationOptions[navigation.currentPage] == option
+                ? AppTheme.primary
+                : const Color.fromARGB(255, 1, 15, 36),
           ),
           label: '',
         );
