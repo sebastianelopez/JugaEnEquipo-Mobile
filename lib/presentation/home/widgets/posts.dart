0136f2dc15d71563da7658f7a94cb2f78d3255ee
sebastianelopez <lopezsebastian.emanuel@gmail.com>
2024-10-09 13:55:08 -0300

    add skeleton to cards while loading

--- lib/presentation/home/widgets/posts.dart
+++ lib/presentation/home/widgets/posts.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:jugaenequipo/datasources/models/post_model.dart';
 import 'package:jugaenequipo/presentation/home/business_logic/home_screen_provider.dart';
 import 'package:jugaenequipo/theme/app_theme.dart';
 import 'package:jugaenequipo/presentation/home/widgets/widgets.dart';
 import 'package:jugaenequipo/global_widgets/widgets.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 import 'package:provider/provider.dart';
 
 class Posts extends StatelessWidget {
@@ -13,19 +15,36 @@ class Posts extends StatelessWidget {
     final homeScreen = Provider.of<HomeScreenProvider>(context);
     final size = MediaQuery.of(context).size;
 
+    final posts = homeScreen.isLoading
+        ? List.filled(
+            7,
+            PostModel(
+              id: '',
+              copy: 'aaaaaaaaaaaaaaaaa',
+              likes: 1,
+              comments: 2,
+              createdAt: '2024-04-20 20:18:04Z',
+              updatedAt: '',
+              deletedAt: '',
+            ))
+        : homeScreen.posts;
+
     return Stack(
       children: [
         RefreshIndicator(
           color: AppTheme.primary,
           onRefresh: homeScreen.onRefresh,
-          child: ListView.builder(
-            controller: homeScreen.scrollController,
-            itemCount: homeScreen.postsmocks.length,
-            itemBuilder: (BuildContext context, int index) {
-              return PostCard(
-                post: homeScreen.postsmocks[index],
-              );
-            },
+          child: Skeletonizer(
+            enabled: homeScreen.isLoading,
+            child: ListView.builder(
+              controller: homeScreen.scrollController,
+              itemCount: posts.length,
+              itemBuilder: (BuildContext context, int index) {
+                return PostCard(
+                  post: posts[index],
+                );
+              },
+            ),
           ),
         ),
         if (homeScreen.isLoading)
