a68d91cd037b0da1ebe5c91d8537627f8de7b42f
sebastianelopez <lopezsebastian.emanuel@gmail.com>
2024-11-02 00:21:34 -0300

    add update profile image funcionality

---
+++ lib/providers/image_picker_provider.dart
@@ -0,0 +1,220 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:jugaenequipo/datasources/api_service.dart';
+import 'package:jugaenequipo/datasources/models/models.dart';
+import 'package:jugaenequipo/datasources/user_use_cases/update_user_profile_image.dart';
+import 'package:jugaenequipo/providers/providers.dart';
+import 'package:mime/mime.dart';
+import 'package:provider/provider.dart';
+
+enum ImageType { post, imageProfile }
+
+class ImagePickerProvider extends ChangeNotifier {
+  List<XFile>? mediaFileList;
+  File? profileImage;
+
+  void _setImageFile(File? value) {
+    profileImage = value;
+  }
+
+  dynamic _pickImageError;
+
+  String? _retrieveDataError;
+
+  final ImagePicker _picker = ImagePicker();
+
+//Image Picker function to get image from gallery
+  Future getImageFromGallery(bool isMulti) async {
+    try {
+      if (isMulti) {
+        final List<XFile> pickedFileList = await _picker.pickMultiImage();
+
+        mediaFileList = pickedFileList;
+      } else {
+        final XFile? pickedFile = await _picker.pickImage(
+          source: ImageSource.gallery,
+        );
+
+        if (pickedFile == null) return;
+
+        final File cleanFile = File(pickedFile.path);
+        _setImageFile(cleanFile);
+      }
+    } catch (e) {
+      _pickImageError = e;
+    }
+  }
+
+  //Image Picker function to get image from camera
+  Future getImageFromCamera() async {
+    try {
+      final XFile? pickedFile = await _picker.pickImage(
+        source: ImageSource.camera,
+      );
+      if (pickedFile == null) return;
+
+      final File cleanFile = File(pickedFile.path);
+      final mimeType = lookupMimeType(cleanFile.path);
+      _setImageFile(cleanFile);
+    } catch (e) {
+      _pickImageError = e;
+    }
+  }
+
+  //Show options to get image from camera or gallery
+  Future showOptions(BuildContext context,
+      {required ImageType imageType, required String userId}) async {
+        var savedContext = context;
+    showDialog(
+      context: savedContext,
+      builder: (context) => SimpleDialog(
+        children: [
+          SimpleDialogOption(
+            child: const Text('Photo Gallery'),
+            onPressed: () async {
+              // close the options modal
+              Navigator.of(context).pop();
+              // get image from gallery
+
+              if (imageType == ImageType.post) {
+                await getImageFromGallery(true);
+              } else if (imageType == ImageType.imageProfile) {
+                getImageFromGallery(false).then((_) async {
+                  if (profileImage == null) return;
+                  final result =
+                      await updateUserProfileImage(userId, profileImage!);
+                  profileImage = null;
+                  // ignore: use_build_context_synchronously
+                   _handleImageSelection(result, savedContext);
+                });
+              }
+            },
+          ),
+          SimpleDialogOption(
+            child: const Text('Camera'),
+            onPressed: () {
+              // close the options modal
+              Navigator.of(context).pop();
+              // get image from camera
+              getImageFromCamera();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _handleImageSelection(Result result, BuildContext context) {
+
+    switch (result) {
+      case Result.success:
+        Provider.of<UserProvider>(context, listen: false).updateUser();
+
+        break;
+      case Result.unauthorized:
+        showDialog(
+          // ignore: use_build_context_synchronously
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Error'),
+              content: const Text('unauthorized'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Aceptar'),
+                ),
+              ],
+            );
+          },
+        );
+        break;
+      case Result.error:
+        showDialog(
+          // ignore: use_build_context_synchronously
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Error'),
+              content: const Text(
+                  'Ocurrió un error inesperado. Por favor, inténtalo de nuevo.'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Aceptar'),
+                ),
+              ],
+            );
+          },
+        );
+
+        break;
+    }
+  }
+
+  Text? _getRetrieveErrorWidget() {
+    if (_retrieveDataError != null) {
+      final Text result = Text(_retrieveDataError!);
+      _retrieveDataError = null;
+      return result;
+    }
+    return null;
+  }
+
+  Widget previewImages() {
+    final Text? retrieveError = _getRetrieveErrorWidget();
+    if (retrieveError != null) {
+      return retrieveError;
+    }
+    if (mediaFileList != null) {
+      return Semantics(
+        label: 'image_picker_example_picked_images',
+        child: GridView.builder(
+          key: UniqueKey(),
+          itemBuilder: (BuildContext context, int index) {
+            return Semantics(
+              label: 'image_picker_example_picked_image',
+              child: kIsWeb
+                  ? Image.network(mediaFileList![index].path)
+                  : Image.file(
+                      height: 80,
+                      width: MediaQuery.of(context).size.width /
+                          mediaFileList!.length,
+                      File(mediaFileList![index].path),
+                      errorBuilder: (BuildContext context, Object error,
+                          StackTrace? stackTrace) {
+                        return const Center(
+                            child: Text('This image type is not supported'));
+                      },
+                    ),
+            );
+          },
+          itemCount: mediaFileList!.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount:
+                mediaFileList!.length, // Adjust the number of columns as needed
+            mainAxisSpacing: 4.0,
+            crossAxisSpacing: 4,
+          ),
+        ),
+      );
+    } else if (_pickImageError != null) {
+      return Text(
+        'Pick image error: $_pickImageError',
+        textAlign: TextAlign.center,
+      );
+    } else {
+      return const Text(
+        'You have not yet picked an image.',
+        textAlign: TextAlign.center,
+      );
+    }
+  }
+}
--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -1,3 +1,4 @@
 export 'package:jugaenequipo/providers/navigation_provider.dart';
 export 'package:jugaenequipo/providers/internalization_provider.dart';
 export 'package:jugaenequipo/providers/user_provider.dart';
+export 'package:jugaenequipo/providers/image_picker_provider.dart';
