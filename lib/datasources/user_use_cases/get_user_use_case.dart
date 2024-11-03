bbdbec80eb5a2bc802c53393baebdc59a2be5b8d
sebastianelopez <lopezsebastian.emanuel@gmail.com>
2024-11-03 15:52:45 -0300

    translate comments to english

--- lib/datasources/user_use_cases/get_user_use_case.dart
+++ lib/datasources/user_use_cases/get_user_use_case.dart
@@ -15,17 +15,17 @@ Future<UserModel?> getUserById(String id) async {
       },
     );
 
-    // Manejar la respuesta
+    // Manage the response
     if (response.statusCode == 200) {
       print('API call successful: ${response.data['data']}');
       return UserModel.fromJson(response.data['data']);
     } else {
-      // Error: Manejar la respuesta de error
+      // Error: Manage the error response
       print('API call failed: ${response.statusMessage}');
       return null;
     }
   } catch (e) {
-    // Error: Manejar errores de red
+    // Error: Manage network errors
     print('Network error occurred: $e');
     return null;
   }
