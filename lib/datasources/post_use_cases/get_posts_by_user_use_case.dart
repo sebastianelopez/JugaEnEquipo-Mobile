181e8ee4a3cb135b818b7419c4175072a9663c34
sebastianelopez <lopezsebastian.emanuel@gmail.com>
2024-11-30 20:02:09 -0300

    print only in debug mode

--- lib/datasources/post_use_cases/get_posts_by_user_use_case.dart
+++ lib/datasources/post_use_cases/get_posts_by_user_use_case.dart
@@ -1,7 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:jugaenequipo/datasources/api_service.dart';
 import 'package:jugaenequipo/datasources/models/models.dart';
-import 'package:jugaenequipo/utils/utils.dart';
 
 Future<List<PostModel>?> getPostsByUserId(String id) async {
   try {
@@ -22,27 +22,37 @@ Future<List<PostModel>?> getPostsByUserId(String id) async {
 
     // Manejar la respuesta
     if (response.statusCode == 200) {
-      debugLog('API call successful: ${response.data['data']}');
+      if (kDebugMode) {
+        debugPrint('API call successful: ${response.data['data']}');
+      }
       final data = response.data['data'];
 
       // Debug: Check if data is a list
       if (data is List) {
-        debugLog('Data is a list');
         final posts = data.map((post) => PostModel.fromJson(post)).toList();
-        debugLog('Parsed posts: $posts');
+        if (kDebugMode) {
+          debugPrint('Data is a list');
+          debugPrint('Parsed posts: $posts');
+        }
         return posts;
       } else {
-        debugLog('Data is not a list');
+        if (kDebugMode) {
+          debugPrint('Data is not a list');
+        }
         return null;
       }
     } else {
       // Error: Manejar la respuesta de error
-      debugLog('API call failed: ${response.statusMessage}');
+      if (kDebugMode) {
+        debugPrint('API call failed: ${response.statusMessage}');
+      }
       return null;
     }
   } catch (e) {
     // Error: Manejar errores de red
-    debugLog('Network error occurredd: $e');
+    if (kDebugMode) {
+      debugPrint('Network error occurredd: $e');
+    }
     return null;
   }
 }
