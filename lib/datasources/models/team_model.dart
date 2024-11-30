c3073675cda9041dc553a90a2a7c9935ac637f86
sebastianelopez <lopezsebastian.emanuel@gmail.com>
2024-11-30 19:19:49 -0300

    improve team model

--- lib/datasources/models/team_model.dart
+++ lib/datasources/models/team_model.dart
@@ -1,26 +1,40 @@
+import 'package:jugaenequipo/datasources/models/models.dart';
+
 class TeamModel {
   final String id;
   final String name;
   final List<String> membersIds;
+  final String? teamImage;
+  final GameModel game;
+  final bool verified;
 
   TeamModel({
     required this.id,
     required this.name,
     required this.membersIds,
+    this.teamImage,
+    required this.game,
+    required this.verified,
   });
 
   factory TeamModel.fromJson(Map<String, dynamic> json) {
     return TeamModel(
         id: json['id'] as String,
         name: json['name'] as String,
-        membersIds: json['membersIds'] as List<String>);
+        membersIds: json['membersIds'] as List<String>,
+        teamImage:
+            json['teamImage'] != null ? json['teamImage'] as String : null,
+        game: GameModel.fromJson(json['game']),
+        verified: json['verified'] as bool);
   }
 
   Map<String, dynamic> toJson() {
     return {
       'id': id,
       'name': name,
-      'membersIds': membersIds
+      'membersIds': membersIds,
+      'teamImage': teamImage,
+      'verified': verified,
     };
   }
 }
