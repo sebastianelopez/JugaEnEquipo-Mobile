380d8af45117795ce03d798ce24a1b658fe1d1ac
sebastianelopez <lopezsebastian.emanuel@gmail.com>
2024-11-30 19:55:30 -0300

    print only in debug mode

--- lib/datasources/post_use_cases/add_post_resource_use_case.dart
+++ lib/datasources/post_use_cases/add_post_resource_use_case.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
-
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:jugaenequipo/datasources/api_service.dart';
 
-Future<void> addPostResource(String postId, String mediaId, File resource, bool isVideo) async {
+Future<void> addPostResource(
+    String postId, String mediaId, File resource, bool isVideo) async {
   try {
     const storage = FlutterSecureStorage();
     final accessToken = await storage.read(key: 'access_token');
@@ -25,13 +27,19 @@ Future<void> addPostResource(String postId, String mediaId, File resource, bool 
     // Handle the response
     if (response.statusCode == 202) {
       // Success: Process the response data
-      print('API call successful: ${response.data}');
+      if (kDebugMode) {
+        debugPrint('API call successful: ${response.data}');
+      }
     } else {
       // Error: Handle the error response
-      print('API call failed: ${response.statusMessage}');
+      if (kDebugMode) {
+        debugPrint('API call failed: ${response.statusMessage}');
+      }
     }
   } catch (e) {
     // Error: Handle network errors
-    print('Network error occurred: $e');
+    if (kDebugMode) {
+      debugPrint('Network error occurred: $e');
+    }
   }
 }
