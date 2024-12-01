c602d90f6bc77074465bd5aa3fc7b572fe9072dc
sebastianelopez <lopezsebastian.emanuel@gmail.com>
2024-11-30 21:52:54 -0300

    replace container for sizedBox

--- lib/presentation/splash/screens/splash_screen.dart
+++ lib/presentation/splash/screens/splash_screen.dart
@@ -65,7 +65,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
+      body: SizedBox(
         width: double.infinity,
         height: double.infinity,
         child: Column(
