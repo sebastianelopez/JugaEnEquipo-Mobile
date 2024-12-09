236853298408dd09777abd2b580e42517483035e
sebastianelopez <lopezsebastian.emanuel@gmail.com>
2024-12-09 20:48:45 -0300

    add profile modal

--- lib/presentation/profile/widgets/widgets.dart
+++ lib/presentation/profile/widgets/widgets.dart
@@ -1,3 +1,4 @@
+export 'package:jugaenequipo/presentation/profile/widgets/profile_modal.dart';
 export 'package:jugaenequipo/presentation/profile/widgets/stats_table.dart';
 export 'package:jugaenequipo/presentation/profile/widgets/profile_avatar.dart';
 export 'package:jugaenequipo/presentation/profile/widgets/profile_elevated_button.dart';
