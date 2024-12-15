22addd3ae57bc7aeb51a4556dc0f0ddab35cdbdb
sebastianelopez <lopezsebastian.emanuel@gmail.com>
2024-12-15 20:20:39 -0300

    get post comments funcionality and add template skeleton

--- lib/presentation/home/widgets/comments_list.dart
+++ lib/presentation/home/widgets/comments_list.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:jugaenequipo/presentation/home/business_logic/home_screen_provider.dart';
+import 'package:jugaenequipo/datasources/models/models.dart';
 import 'package:jugaenequipo/providers/providers.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class CommentsList extends StatelessWidget {
   const CommentsList({
@@ -11,77 +12,89 @@ class CommentsList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final homeProvider = Provider.of<HomeScreenProvider>(context);
     final user = Provider.of<UserProvider>(context).user;
+    final postProvider = Provider.of<PostProvider>(context);
+
+    final comments = postProvider.isLoading
+        ? List.filled(
+            7,
+            CommentModel(
+              id: '',
+              user: 'user',
+              copy: 'aaaaaaaaaaaaaaaaa',
+              createdAt: '2024-04-20 20:18:04Z',
+            ))
+        : postProvider.comments;
 
     return SingleChildScrollView(
       padding: const EdgeInsets.only(bottom: 50.0),
-      child: Column(
-        children: List.generate(homeProvider.commentsMocks.length, (index) {
-          final isLoggedUser =
-              homeProvider.commentsMocks[index].userId == user?.id;
-          return Container(
-            padding:
-                const EdgeInsets.only(left: 16, right: 16, top: 10, bottom: 10),
-            child: Align(
-              alignment:
-                  (!isLoggedUser ? Alignment.topLeft : Alignment.topRight),
-              child: Column(
-                crossAxisAlignment:
-                    homeProvider.commentsMocks[index].userId != user?.id
-                        ? CrossAxisAlignment.start
-                        : CrossAxisAlignment.end,
-                children: [
-                  Text(
-                      (user?.firstName != null && user?.lastName != null)
-                          ? '${user!.firstName} ${user.lastName}'
-                          : '',
-                      style: TextStyle(
-                          fontSize: 15.h, fontWeight: FontWeight.bold)),
-                  Row(
-                    mainAxisAlignment: !isLoggedUser
-                        ? MainAxisAlignment.start
-                        : MainAxisAlignment.end,
-                    children: [
-                      if (!isLoggedUser)
-                        CircleAvatar(
-                          backgroundImage: const AssetImage(
-                              'assets/login.png'), // Replace with your profile image path
-                          radius: 16.w,
-                          backgroundColor: Colors.white,
+      child: Skeletonizer(
+        enabled: postProvider.isLoading,
+        child: Column(
+          children: List.generate(comments.length, (index) {
+            final isLoggedUser = comments[index].id == user?.id;
+            return Container(
+              padding: const EdgeInsets.only(
+                  left: 16, right: 16, top: 10, bottom: 10),
+              child: Align(
+                alignment:
+                    (!isLoggedUser ? Alignment.topLeft : Alignment.topRight),
+                child: Column(
+                  crossAxisAlignment: comments[index].id != user?.id
+                      ? CrossAxisAlignment.start
+                      : CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                        (user?.firstName != null && user?.lastName != null)
+                            ? '${user!.firstName} ${user.lastName}'
+                            : '',
+                        style: TextStyle(
+                            fontSize: 15.h, fontWeight: FontWeight.bold)),
+                    Row(
+                      mainAxisAlignment: !isLoggedUser
+                          ? MainAxisAlignment.start
+                          : MainAxisAlignment.end,
+                      children: [
+                        if (!isLoggedUser)
+                          CircleAvatar(
+                            backgroundImage:
+                                const AssetImage('assets/login.png'),
+                            radius: 16.w,
+                            backgroundColor: Colors.white,
+                          ),
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: (!isLoggedUser
+                                ? Colors.grey.shade300
+                                : Colors.blue[200]),
+                          ),
+                          margin: EdgeInsets.only(
+                              left: isLoggedUser ? 0 : 10,
+                              right: isLoggedUser ? 10 : 0,
+                              top: 7),
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 10, horizontal: 16),
+                          child: Text(
+                            comments[index].copy ?? '',
+                            style: TextStyle(fontSize: 15.h),
+                          ),
                         ),
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: (!isLoggedUser
-                              ? Colors.grey.shade300
-                              : Colors.blue[200]),
-                        ),
-                        margin: EdgeInsets.only(
-                            left: isLoggedUser ? 0 : 10,
-                            right: isLoggedUser ? 10 : 0,
-                            top: 7),
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 10, horizontal: 16),
-                        child: Text(
-                          homeProvider.commentsMocks[index].copy ?? '',
-                          style: TextStyle(fontSize: 15.h),
-                        ),
-                      ),
-                      if (isLoggedUser)
-                        CircleAvatar(
-                          backgroundImage: const AssetImage(
-                              'assets/login.png'), // Replace with your profile image path
-                          radius: 16.w,
-                          backgroundColor: Colors.white,
-                        ),
-                    ],
-                  ),
-                ],
+                        if (isLoggedUser)
+                          CircleAvatar(
+                            backgroundImage:
+                                const AssetImage('assets/login.png'),
+                            radius: 16.w,
+                            backgroundColor: Colors.white,
+                          ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-          );
-        }),
+            );
+          }),
+        ),
       ),
     );
   }
