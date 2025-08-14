687fe2f9c1ce956d566ebc1d8e18079c62a24999
sebastianelopez <lopezsebastian.emanuel@gmail.com>
2025-08-13 21:23:29 -0300

    add get user by username api call

---
+++ lib/datasources/user_use_cases/get_users_by_username_use_case.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:jugaenequipo/datasources/api_service.dart';
+import 'package:jugaenequipo/datasources/models/models.dart';
+
+Future<List<UserModel>?> getUsersByUsername(String username) async {
+  try {
+    const storage = FlutterSecureStorage();
+    final accessToken = await storage.read(key: 'access_token');
+
+    final response = await APIService.instance.request(
+      '/api/users',
+      DioMethod.get,
+      contentType: 'application/json',
+      param: {
+        'q': 'username:$username',
+      },
+      headers: {
+        'Authorization': 'Bearer $accessToken',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      if (kDebugMode) {
+        debugPrint(
+            'getUsersByUsername - API call successful: ${response.data['data']}');
+      }
+
+      final data = response.data['data'];
+      if (data is List) {
+        return data.map<UserModel>((json) => UserModel.fromJson(json)).toList();
+      }
+
+      if (kDebugMode) {
+        debugPrint('getUsersByUsername - Unexpected data format');
+      }
+      return null;
+    } else {
+      if (kDebugMode) {
+        debugPrint(
+            'getUsersByUsername - API call failed: ${response.statusCode} ${response.statusMessage}');
+      }
+      return null;
+    }
+  } catch (e) {
+    if (kDebugMode) {
+      debugPrint('getUsersByUsername - Network error occurred: $e');
+    }
+    return null;
+  }
+}
