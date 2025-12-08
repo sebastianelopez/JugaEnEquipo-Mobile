a4cae7ba0cae61aa539824a3396d87dce1456d72
sebastianelopez <lopezsebastian.emanuel@gmail.com>
2025-12-08 00:37:10 -0300

    fix social netwroks aligment and make them open the app

--- lib/presentation/profile/widgets/social_media_section.dart
+++ lib/presentation/profile/widgets/social_media_section.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:jugaenequipo/theme/app_theme.dart';
 import 'package:jugaenequipo/l10n/app_localizations.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SocialMediaSection extends StatelessWidget {
   final Map<String, String> socialLinks;
@@ -15,6 +16,7 @@ class SocialMediaSection extends StatelessWidget {
     }
 
     return Container(
+      alignment: Alignment.centerLeft,
       margin: EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -45,15 +47,8 @@ class SocialMediaSection extends StatelessWidget {
     final color = _getColorForPlatform(platform);
 
     return GestureDetector(
-      onTap: () {
-        // TODO: Implement URL launcher when package is added
-        // For now, show a snackbar
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text('Opening $platform: $url'),
-            duration: const Duration(seconds: 2),
-          ),
-        );
+      onTap: () async {
+        await _launchUrl(url, context);
       },
       child: Container(
         padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 12.h),
@@ -129,6 +124,42 @@ class SocialMediaSection extends StatelessWidget {
         return Colors.blue;
       default:
         return AppTheme.primary;
+    }
+  }
+
+  Future<void> _launchUrl(String urlString, BuildContext context) async {
+    try {
+      // Asegurar que la URL tenga el protocolo correcto
+      Uri uri;
+      if (urlString.startsWith('http://') || urlString.startsWith('https://')) {
+        uri = Uri.parse(urlString);
+      } else {
+        uri = Uri.parse('https://$urlString');
+      }
+
+      // Intentar abrir la URL directamente
+      final launched = await launchUrl(
+        uri,
+        mode: LaunchMode.externalApplication,
+      );
+
+      if (!launched && context.mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(AppLocalizations.of(context)!.errorMessage),
+            duration: const Duration(seconds: 2),
+          ),
+        );
+      }
+    } catch (e) {
+      if (context.mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('${AppLocalizations.of(context)!.errorMessage}: $e'),
+            duration: const Duration(seconds: 3),
+          ),
+        );
+      }
     }
   }
 }
